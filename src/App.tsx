c1513890f7d4d1a6cb6d2ebb98f344b64be0064e
Eugen Aronsky <zolotareveugen.work@gmail.com>
2024-08-13 22:40:54 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
-import { useState } from "react";
-
 const WebApp = window.Telegram.WebApp;
 const user = { ...window.Telegram.WebApp.initDataUnsafe };
 
 function App() {
-  const [count, setCount] = useState(0);
   console.log(WebApp, user);
   return (
     <main className="flex w-full h-full bg-telegram-black">
-      <div>
-        <div className="text-white">{JSON.stringify(user)}</div>
-      </div>
+      <div className="text-white">{JSON.stringify(user)}</div>
     </main>
   );
 }
