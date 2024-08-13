d39cd7220b584b71285582ea90156ff09349b4a2
Eugen Aronsky <zolotareveugen.work@gmail.com>
2024-08-14 00:27:16 +0300

    fix

--- src/pages/Recommendations.tsx
+++ src/pages/Recommendations.tsx
@@ -15,37 +15,37 @@ const Recommendations = () => {
       <h1 className="text-telegram-black font-bold text-xl bg-telegram-white fixed py-3 w-full text-center z-10">
         Relevant Apartments
       </h1>
-      <section className="flex flex-col w-full h-fit mt-[52px] bg-red-400 p-3 gap-3">
+      <section className="flex flex-col w-full h-fit mt-[52px] p-3 gap-3 bg-telegram-secondary-white">
         {Array.from({ length: 3 }).map(() => (
-          <Card>
+          <Card className="bg-telegram-white shadow-black/30 shadow-lg">
             <CardHeader className="p-3">
               <CarouselWithDots />
-              <CardTitle className="!mt-3 flex justify-between text-xl">
+              <CardTitle className="!mt-3 flex justify-between text-lg">
                 <a
                   href="https://maps.google.com/maps?daddr=<lat>,<long>&amp;ll="
                   className="flex gap-2 text-telegram-link items-center underline underline-offset-4"
                 >
-                  <MapPinned />
+                  <MapPinned size={22} />
                   <p>Уша 13 Кирьят Моцкин</p>
                 </a>
                 4300 ₪
               </CardTitle>
             </CardHeader>
-            <CardContent className="px-3 py-1.5 grid grid-cols-2">
+            <CardContent className="px-3 py-1">
               <span className="flex gap-2 items-center">
-                <Pin size={18} />
+                <Pin size={18} className="stroke-black fill-red-500" />
                 <p>4 комнаты</p>
               </span>
               <span className="flex gap-2 items-center">
-                <Pin size={18} />
+                <Pin size={18} className="stroke-black fill-red-500" />
                 <p>1/1 этаж</p>
               </span>
               <span className="flex gap-2 items-center">
-                <Pin size={18} />
+                <Pin size={18} className="stroke-black fill-red-500" />
                 <p>вход с 1.10</p>
               </span>
               <span className="flex gap-2 items-center">
-                <Pin size={18} />
+                <Pin size={18} className="stroke-black fill-red-500" />
                 <p>меблирована</p>
               </span>
             </CardContent>
