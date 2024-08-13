6f22fc627be296bd00bccf265cf967c6f834b7b7
Eugen Aronsky <zolotareveugen.work@gmail.com>
2024-08-13 22:56:50 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import { Button } from "./components/ui/button";
 import useTelegram from "./hooks/useTelegram";
 
 function App() {
-  const { onClose, onToggleButton } = useTelegram();
+  const { onClose, onToggleButton, user } = useTelegram();
   return (
     <main className="flex w-full h-full bg-telegram-black">
+      <h1 className="text-telegram-white">{user?.username}</h1>
       <Button onClick={onClose}>Close</Button>
       <Button onClick={onToggleButton}>Toggle</Button>
     </main>
