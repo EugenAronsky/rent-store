e4628f79cfb6af1d7e0dfd99365eeb08a36db64e
Eugen Aronsky <zolotareveugen.work@gmail.com>
2024-08-14 00:18:51 +0300

    test

--- src/components/ui/custom/carousel-with-dots.tsx
+++ src/components/ui/custom/carousel-with-dots.tsx
@@ -6,6 +6,11 @@ import {
 } from "@/components/ui/carousel";
 import { cn } from "@/lib/utils";
 import React from "react";
+import pic1 from "../../../assets/photo_1_2024-08-13_23-59-04.jpg";
+import pic2 from "../../../assets/photo_2_2024-08-13_23-59-04.jpg";
+import pic3 from "../../../assets/photo_3_2024-08-13_23-59-04.jpg";
+import pic4 from "../../../assets/photo_4_2024-08-13_23-59-04.jpg";
+import pic5 from "../../../assets/photo_5_2024-08-13_23-59-04.jpg";
 
 export function CarouselWithDots() {
   const [api, setApi] = React.useState<CarouselApi>();
@@ -25,21 +30,37 @@ export function CarouselWithDots() {
 
   return (
     <Carousel setApi={setApi} className="rounded-md overflow-hidden">
-      <CarouselContent className="*:w-full *:bg-telegram-hint">
-        <CarouselItem className="aspect-square">...</CarouselItem>
-        <CarouselItem className="aspect-square">...</CarouselItem>
-        <CarouselItem className="aspect-square">...</CarouselItem>
+      <CarouselContent className="*:w-full">
+        <CarouselItem className="aspect-square">
+          <img className="object-cover w-full h-full" src={pic1} />
+        </CarouselItem>{" "}
+        <CarouselItem className="aspect-square">
+          <img className="object-cover w-full h-full" src={pic2} />
+        </CarouselItem>{" "}
+        <CarouselItem className="aspect-square">
+          <img className="object-cover w-full h-full" src={pic3} />
+        </CarouselItem>{" "}
+        <CarouselItem className="aspect-square">
+          <img className="object-cover w-full h-full" src={pic4} />
+        </CarouselItem>{" "}
+        <CarouselItem className="aspect-square">
+          <img className="object-cover w-full h-full" src={pic5} />
+        </CarouselItem>
       </CarouselContent>
+
       <div className="w-full flex justify-center gap-2 py-2 absolute bottom-0 *:rounded-full">
-        {Array.from({ length: 3 }).map((_, index) => (
-          <button
-            onClick={() => api?.scrollTo(index)}
-            className={cn(
-              "h-2 w-2 bg-telegram-black",
-              index === current && "bg-telegram-link"
-            )}
-          />
-        ))}
+        {api &&
+          api
+            .slideNodes()
+            .map((_, index) => (
+              <button
+                onClick={() => api?.scrollTo(index)}
+                className={cn(
+                  "h-1.5 w-1.5 bg-telegram-hint opacity-80",
+                  index === current && "bg-telegram-primary-text opacity-100"
+                )}
+              />
+            ))}
       </div>
     </Carousel>
   );
--- src/pages/Recommendations.tsx
+++ src/pages/Recommendations.tsx
@@ -2,12 +2,13 @@ import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
+  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
 import { CarouselWithDots } from "@/components/ui/custom/carousel-with-dots";
-import { MapPinned, PenBox, PhoneCall } from "lucide-react";
+import { MapPinned, PenBox, PhoneCall, Pin } from "lucide-react";
 
 const Recommendations = () => {
   return (
@@ -15,34 +16,52 @@ const Recommendations = () => {
       <h1 className="text-telegram-black font-bold text-xl bg-telegram-white fixed py-3 w-full text-center z-10">
         Relevant Apartments
       </h1>
-      <section className="w-full min-h-screen mt-[52px] bg-red-400 p-3">
-        <Card className="*:p-3">
-          <CardHeader>
-            <CarouselWithDots />
-            <CardTitle>4300 ₪</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <a
-              href="geo:37.786971,-122.399677"
-              className="flex gap-2 text-telegram-link"
-            >
-              <MapPinned size={18} />
-              <p>Уша 13 Кирьят Моцкин</p>{" "}
-            </a>
-            <p>4 комнаты</p>
-            <p>1/1 этаж</p>
-          </CardContent>
-          <CardFooter className="flex gap-3">
-            <Button className="flex gap-2 items-center w-full">
-              <span>Позвонить</span>
-              <PhoneCall size={18} />
-            </Button>
-            <Button className="flex gap-2 items-center w-full">
-              <span>Написать</span>
-              <PenBox size={18} />
-            </Button>
-          </CardFooter>
-        </Card>
+      <section className="flex flex-col w-full h-fit mt-[52px] bg-red-400 p-3 gap-3">
+        {Array.from({ length: 3 }).map(() => (
+          <Card>
+            <CardHeader className="p-3">
+              <CarouselWithDots />
+              <CardTitle className="!mt-3 flex justify-between text-xl">
+                <a
+                  href="https://maps.google.com/maps?daddr=<lat>,<long>&amp;ll="
+                  className="flex gap-2 text-telegram-link items-center underline underline-offset-4"
+                >
+                  <MapPinned />
+                  <p>Уша 13 Кирьят Моцкин</p>
+                </a>
+                4300 ₪
+              </CardTitle>
+            </CardHeader>
+            <CardContent className="px-3 py-1.5 grid grid-cols-2">
+              <span className="flex gap-2 items-center">
+                <Pin size={18} />
+                <p>4 комнаты</p>
+              </span>
+              <span className="flex gap-2 items-center">
+                <Pin size={18} />
+                <p>1/1 этаж</p>
+              </span>
+              <span className="flex gap-2 items-center">
+                <Pin size={18} />
+                <p>вход с 1.10</p>
+              </span>
+              <span className="flex gap-2 items-center">
+                <Pin size={18} />
+                <p>меблирована</p>
+              </span>
+            </CardContent>
+            <CardFooter className="flex gap-3 p-3">
+              <Button className="flex gap-2 items-center w-full">
+                <a href="tel:0535242201">Позвонить</a>
+                <PhoneCall size={18} />
+              </Button>
+              <Button className="flex gap-2 items-center w-full">
+                <a href="https://t.me/@sergioboiko">Написать</a>
+                <PenBox size={18} />
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
       </section>
     </main>
   );
